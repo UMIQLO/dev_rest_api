61e85e8c407c4d61b5c2a744accebcb8f6db8162
David Lo <umiqlo.moe@gmail.com>
2018-02-21 17:44:57 +0800

    add to_representation function

--- mp_rest_api/serializers.py
+++ mp_rest_api/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import *
 from rest_framework.validators import UniqueValidator
+from util.CommonUtil import *
 
-# class UserSerializer(serializers.ModelSerializer):
-    # class Meta:
-        # model = User
-        # fields = '__all__'
+class ToUpperCaseCharField(serializers.CharField):
+    def to_representation(self, value):
+        return ''
+
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
             required=True,
@@ -14,13 +15,20 @@ class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
             validators=[UniqueValidator(queryset=User.objects.all())]
     )
-    password = serializers.CharField(min_length=8)
-    first_name = serializers.CharField(min_length=8)
-    last_name = serializers.CharField(min_length=8)
+    first_name = ToUpperCaseCharField()
+    # password = serializers.CharField(min_length=8)
+    # first_name = serializers.CharField()
+    # last_name = serializers.CharField()
     
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+             validated_data['password'])
+        return user
+
     class Meta:
         model = User
-        fields = '__all__'
+        # field to display
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
         
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
