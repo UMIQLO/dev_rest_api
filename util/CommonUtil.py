146ccdce01d6cdefdcf32d35b8958464f3ceec84
David Lo <umiqlo.moe@gmail.com>
2018-02-21 17:42:42 +0800

    add util class

---
+++ util/CommonUtil.py
@@ -0,0 +1,15 @@
+import json
+
+def current_timestamp():
+    import calendar;
+    import time;
+    ts = calendar.timegm(time.gmtime())
+    return ts
+
+def json_encode(data, status=False):
+    response = {
+        "success": status,
+        "server_time": current_timestamp(),
+        "response": data,
+    }
+    return response
