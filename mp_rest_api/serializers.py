c950ba2a011b37d959b227beb61a2b8f59a1fbb2
David Lo <umiqlo.moe@gmail.com>
2018-02-26 17:26:50 +0800

    update json output format

--- mp_rest_api/serializers.py
+++ mp_rest_api/serializers.py
@@ -3,9 +3,11 @@ from .models import *
 from rest_framework.validators import UniqueValidator
 from util.CommonUtil import *
 
+import string
+
 class ToUpperCaseCharField(serializers.CharField):
     def to_representation(self, value):
-        return ''
+        return string.capwords(value)
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -16,15 +18,27 @@ class UserSerializer(serializers.ModelSerializer):
             validators=[UniqueValidator(queryset=User.objects.all())]
     )
     first_name = ToUpperCaseCharField()
-    # password = serializers.CharField(min_length=8)
-    # first_name = serializers.CharField()
-    # last_name = serializers.CharField()
-    
+    last_name = ToUpperCaseCharField() # last_name = serializers.CharField()
+
+    # override original create method
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'],
              validated_data['password'])
         return user
+    
+    # change each item displayed format / style
+    def to_representation(self, user):
+        data = super().to_representation(user)  # the original data
 
+        return {
+            '$jason': {
+                'head': {
+                    
+                },
+                'body': data,
+            }
+        }
+    
     class Meta:
         model = User
         # field to display
