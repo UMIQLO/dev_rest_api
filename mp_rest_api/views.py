2ac7a7dbdacc5c0a478568bcc5274b284c053116
David Lo <umiqlo.moe@gmail.com>
2018-02-26 17:26:32 +0800

    update views

--- mp_rest_api/views.py
+++ mp_rest_api/views.py
@@ -8,6 +8,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
+    ### custom json output format (retrieve & list)
+    # def retrieve(self, request, *args, **kwargs):
+        # return Response({'something': 'my custom JSON retrieve'})
+    # def list(self, request, *args, **kwargs):
+        # return Response({'something': 'list'})
+    
 class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
