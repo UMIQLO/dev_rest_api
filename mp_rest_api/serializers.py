dd6ce6a792802634c04d51eba0fb0fa8fb21dabb
David Lo <umiqlo.moe@gmail.com>
2018-02-27 11:08:49 +0800

    rename the function & tidy the code

--- mp_rest_api/serializers.py
+++ mp_rest_api/serializers.py
@@ -5,11 +5,17 @@ from util.CommonUtil import *
 
 import string
 
-class ToUpperCaseCharField(serializers.CharField):
+# upper case the first character
+class ToUpperCaseFirstChar(serializers.CharField):
     def to_representation(self, value):
         return string.capwords(value)
 
 class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # field to display
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
+        
     email = serializers.EmailField(
             required=True,
             validators=[UniqueValidator(queryset=User.objects.all())]
@@ -17,33 +23,24 @@ class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
             validators=[UniqueValidator(queryset=User.objects.all())]
     )
-    first_name = ToUpperCaseCharField()
-    last_name = ToUpperCaseCharField() # last_name = serializers.CharField()
+    first_name = ToUpperCaseFirstChar()
+    last_name = ToUpperCaseFirstChar() # last_name = serializers.CharField()
 
-    # override original create method
+    # override original "create" method
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'],
              validated_data['password'])
         return user
     
     # change each item displayed format / style
-    def to_representation(self, user):
-        data = super().to_representation(user)  # the original data
+    # def to_representation(self, user):
+        # data = super().to_representation(user)  # get original data
+        # formated_data = {
+            # "status": "ok",
+            # "data" : data,
+        # }
+        # return formated_data
 
-        return {
-            '$jason': {
-                'head': {
-                    
-                },
-                'body': data,
-            }
-        }
-    
-    class Meta:
-        model = User
-        # field to display
-        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
-        
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
