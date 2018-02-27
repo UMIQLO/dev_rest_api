eadac3d51af090651c6196150505634c0f69538a
David Lo <umiqlo.moe@gmail.com>
2018-02-27 11:08:01 +0800

    custom json result output format

--- mp_rest_api/views.py
+++ mp_rest_api/views.py
@@ -3,17 +3,45 @@ from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
+from util.CommonUtil import *
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
-    ### custom json output format (retrieve & list)
-    # def retrieve(self, request, *args, **kwargs):
-        # return Response({'something': 'my custom JSON retrieve'})
-    # def list(self, request, *args, **kwargs):
-        # return Response({'something': 'list'})
+    def list(self, request, *args, **kwargs):
+        response_data = super(UserViewSet, self).list(request, *args, **kwargs)
+        return Response(json_encode(response_data.data, True))
+    # create function in serializers
+    # def create(self, request, *args, **kwargs):
+        # response_data = super(UserViewSet, self).create(request, *args, **kwargs)
+        # return Response(json_encode(response_data.data, True))
+    def retrieve(self, request, *args, **kwargs):
+        response_data = super(UserViewSet, self).retrieve(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
+    def update(self, request, *args, **kwargs):
+        response_data = super(UserViewSet, self).update(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
+    def destroy(self, request, *args, **kwargs):
+        response_data = super(UserViewSet, self).destroy(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
     
 class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+    
+    def list(self, request, *args, **kwargs):
+        response_data = super(MusicViewSet, self).list(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
+    def create(self, request, *args, **kwargs):
+        response_data = super(MusicViewSet, self).create(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
+    def retrieve(self, request, *args, **kwargs):
+        response_data = super(MusicViewSet, self).retrieve(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
+    def update(self, request, *args, **kwargs):
+        response_data = super(MusicViewSet, self).update(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
+    def destroy(self, request, *args, **kwargs):
+        response_data = super(MusicViewSet, self).destroy(request, *args, **kwargs)
+        return Response(json_encode(response_data.data))
